845f179cb20ae292aff1b63fecea51920776cf4d
mtboxing <mtboxing@users.noreply.github.com>
2020-01-28 13:49:39 +0630

    get started

--- index.js
+++ index.js
@@ -122,6 +122,33 @@ app.get('/',(req,res)=>{
 	res.send("Hello World")
 })
 
+app.get('/setgsbutton',function(req,res){
+    setupGetStartedButton(res);    
+});
+
+function setupGetStartedButton(res){
+        var messageData = {
+                "get_started":{"payload":"USER_DEFINED_PAYLOAD"}                
+        };
+        // Start the request
+        request({
+            url: 'https://graph.facebook.com/v2.6/me/messenger_profile?access_token='+ PAGE_ACCESS_TOKEN,
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            form: messageData
+        },
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                // Print out the response body
+                res.send(body);
+
+            } else { 
+                // TODO: Handle errors
+                res.send(body);
+            }
+        });
+    } 
+
 
 
 
