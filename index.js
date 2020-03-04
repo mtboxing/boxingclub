853470f99a0d60064898a749e6b6bf82606a7319
mtboxing <mtboxing@users.noreply.github.com>
2020-03-04 11:30:39 +0630

    asdf

--- index.js
+++ index.js
@@ -419,7 +419,20 @@ app.post('/webhook', (req, res) => {
               {
               "text":"We will notify you if you got challenged on that day."
               }
-            }); 
+            });
+
+            request({
+                "uri": "https://graph.facebook.com/v6.0/me/messages",
+                "qs": { "access_token": PAGE_ACCESS_TOKEN },
+                "method": "POST",
+                "json": request_body
+              }, (err, res, body) => {
+                if (!err) { 
+                  console.log('message sent!');
+                } else {
+                  console.error("Unable to send message:" + err);
+                }
+              });  
           }
 
       	}
